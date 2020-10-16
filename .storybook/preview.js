3dcd06e5bb9db2a2288c11b297222e809be4db0b
Michael Pautov <michael.pautov@gmail.com>
2020-10-16 13:21:34 +0600

    test

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import { withGlobalStyle } from '../src/decorators'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 }
+
+export const decorators = [
+  withGlobalStyle
+]
