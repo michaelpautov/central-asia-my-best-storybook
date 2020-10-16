d17f0fce3e0614f0d302b2aa4d02a4a76a51ae97
Michael Pautov <michael.pautov@gmail.com>
2020-10-16 10:56:57 +0600

    test

--- src/components/app/app.stories.tsx
+++ src/components/app/app.stories.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
 import App from './app'
-import { Button } from '@devfest/components'
 
 export default {
   title: 'App',
   component: App,
 } as Meta
 
-const Template: Story = (args) => <div><Button/><App {...args} /></div>
+const Template: Story = (args) => <App {...args} />
 
 export const Default = Template.bind({})
 Default.args = {}
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,16 +1,12 @@
-import React, { Fragment } from 'react'
-import { GlobalStyle } from '@devfest/components'
+import React from 'react'
 
 function App() {
   return (
-    <Fragment>
-      <GlobalStyle />
-      <div className="App">
-        <header className="App-header">
-          <p>Asia Dev Fest 2020</p>
-        </header>
-      </div>
-    </Fragment>
+    <div className="App">
+      <header className="App-header">
+        <p>Asia Dev Fest 2020</p>
+      </header>
+    </div>
   )
 }
 
